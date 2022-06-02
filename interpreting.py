30455d9e3a48714a7b9d497e8ae8624d6faf06d7
Rakshhit-Singh <72254094+Rakshhit-Singh@users.noreply.github.com>
2022-06-02 13:26:13 +0530

    Update interpreting.py

--- interpreting.py
+++ interpreting.py
@@ -2,11 +2,11 @@ import pandas as pd
 import plotly.express as px
 
 df  = pd.read_csv("habitable.csv")
-star_name_list = df["Star_name"]
+star_name_list = df["Star Name"]
 mass_list = df["Mass"]
 radius_list = df["Radius"]
 distance_list = df["Distance"]
-gravity_list = df["gravity"]
+gravity_list = df["Gravity"]
 
 fig1 = px.bar(x=star_name_list, y=radius_list) 
 fig1.show()
