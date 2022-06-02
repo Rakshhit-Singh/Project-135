65e7c2fbdf7f9103e45f3c69f7e186caa5871b03
Rakshhit-Singh <72254094+Rakshhit-Singh@users.noreply.github.com>
2022-06-02 13:25:16 +0530

    Add files via upload

---
+++ interpreting.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import plotly.express as px
+
+df  = pd.read_csv("habitable.csv")
+star_name_list = df["Star_name"]
+mass_list = df["Mass"]
+radius_list = df["Radius"]
+distance_list = df["Distance"]
+gravity_list = df["gravity"]
+
+fig1 = px.bar(x=star_name_list, y=radius_list) 
+fig1.show()
+fig2 = px.bar(x=star_name_list, y=mass_list) 
+fig2.show()
+fig3 = px.bar(x=star_name_list, y=distance_list) 
+fig3.show()
+fig4 = px.bar(x=star_name_list, y=gravity_list) 
+fig4.show()
